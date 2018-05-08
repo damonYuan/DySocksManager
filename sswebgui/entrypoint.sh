79af10a8907f04a03adccaff4f0b695c7f469ba0
damonYuan <yzmhit@gmail.com>
2018-05-08 14:43:57 +0800

    update webgui for alipay

--- sswebgui/entrypoint.sh
+++ sswebgui/entrypoint.sh
@@ -10,6 +10,11 @@ sed -i -e "s/WEBGUI_EMAIL_USERNAME/$WEBGUI_EMAIL_USERNAME/g" /root/.ssmgr/webgui
   sed -i -e "s/WEBGUI_DB_NAME/$WEBGUI_DB_NAME/g" /root/.ssmgr/webgui.yml && \
   sed -i -e "s/MANAGER_PASSWORD/$MANAGER_PASSWORD/g" /root/.ssmgr/webgui.yml && \
   sed -i -e "s/MANAGER_ADDRESS/$MANAGER_ADDRESS/g" /root/.ssmgr/webgui.yml && \
-  sed -i -e "s/MANAGER_PORT/$MANAGER_PORT/g" /root/.ssmgr/webgui.yml
+  sed -i -e "s/MANAGER_PORT/$MANAGER_PORT/g" /root/.ssmgr/webgui.yml && \
+  sed -i -e "s/WEBGUI_ALIPAY_APPID/$WEBGUI_ALIPAY_APPID/g" /root/.ssmgr/webgui.yml && \
+  sed -i -e "s/WEBGUI_ALIPAY_NOTIFY_URL/$WEBGUI_ALIPAY_NOTIFY_URL/g" /root/.ssmgr/webgui.yml && \
+  sed -i -e "s/WEBGUI_ALIPAY_MERCHANT_PRIVATE_KEY/$WEBGUI_ALIPAY_MERCHANT_PRIVATE_KEY/g" /root/.ssmgr/webgui.yml && \
+  sed -i -e "s/WEBGUI_ALIPAY_ALIPAY_PUBLIC_KEY/$WEBGUI_ALIPAY_ALIPAY_PUBLIC_KEY/g" /root/.ssmgr/webgui.yml && \
+  sed -i -e "s/WEBGUI_ALIPAY_GATEWAY_URL/$WEBGUI_ALIPAY_GATEWAY_URL/g" /root/.ssmgr/webgui.yml
 
-/usr/bin/ssmgr -c /root/.ssmgr/webgui.yml --debug
+/usr/bin/ssmgr -c /root/.ssmgr/webgui.yml
